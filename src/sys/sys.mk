f314070aa574c2058960d4e9918d3b9f9c6721fc
Romwierz <miczu47@gmail.com>
2025-03-20 21:39:39 +0100

    add dirs to be searched for header files

--- src/sys/sys.mk
+++ src/sys/sys.mk
@@ -3,14 +3,24 @@
 # LINKER_SCRIPT := path to linker script file
 # TARGET_BINARY := path to binary file without extension
 
-# Compiler and flags
 CC = arm-none-eabi-gcc
 
 OPT = -g -Og -gdwarf-2
 CPU = -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16
-CFLAGS = $(CPU) $(OPT) -DSTM32L476xx 
+
+DEFINES := STM32L476xx
+DEFINES_FLAGS := $(addprefix -D,$(DEFINES))
+
+# Add directiories to be searched for header files by the GCC
+INC_DIRS := sys/CMSIS
+# Add a prefix to INC_DIRS. So moduleA would become -ImoduleA. GCC understands this -I flag
+INC_FLAGS := $(addprefix -I,$(INC_DIRS))
+
+CFLAGS = $(CPU) $(OPT) $(DEFINES_FLAGS) $(INC_FLAGS)
 CFLAGS += -Wpedantic -Wall
 # CFLAGS += -Werror
+
 ASFLAGS = $(CPU) $(OPT)
 # ASFLAGS += -Werror
+
 LDFLAGS = -specs=nosys.specs -T$(LINKER_SCRIPT) -Wl,-Map,$(TARGET_BINARY).map,--no-warn-rwx-segment $(CPU) $(OPT)
